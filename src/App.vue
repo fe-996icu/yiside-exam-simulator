bedf9799288cd397a0717ac1c074e859caf3f3de
zhangjianzhong@yiside.com <zhangjianzhong@yiside.com>
2022-02-08 10:03:10 +0800

    antd国际化-汉化

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-	<router-view></router-view>
+	<!-- 国际化 -->
+	<a-config-provider :locale="locale">
+		<router-view></router-view>
+	</a-config-provider>
 </template>
+
+<script lang="ts" setup>
+	// 汉化，ant-design-vue 目前的默认文案是英文，如果需要使用其他语言，可以参考下面的方案。
+	import zhCN from 'ant-design-vue/es/locale/zh_CN'
+
+	const locale = zhCN;
+</script>
