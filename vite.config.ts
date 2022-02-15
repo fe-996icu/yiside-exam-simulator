793fd87ef79ed47d8574bbda4ec3fde012c5290d
zhangjianzhong@yiside.com <zhangjianzhong@yiside.com>
2022-02-15 16:57:28 +0800

    vue编译配置路径别名@

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
-
 // https://next.antdv.com/docs/vue/getting-started-cn#按需加载
 // 如果你使用的 Vite，你可以使用 unplugin-vue-components 来进行按需加载。但是此插件无法处理非组件模块，如 message，这种组件需要手动加载：
 // import { message } from 'ant-design-vue';
@@ -11,6 +10,11 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
 	plugins: [
 		vue(),
 		Components({
